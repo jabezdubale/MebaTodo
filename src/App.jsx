9ba6d8299aa52a4582362bdb37b98bad38c95a0c
jabezdubale <jabezdubale6@gmail.com>
2023-08-10 18:05:45 +0300

    First commit for Meba Todo App. Created a login page for the app.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./app.scss";
+import axios from "axios";
+import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,51 @@
+import "./login.scss";
+import leftellipse from "../../assets/Login/login_Ellipse.png";
+import leftellipse2 from "../../assets/Login/login_Ellipse2.png";
+import rightellipse3 from "../../assets/Login/login_Ellipse3.png";
+import topellipse from "../../assets/Login/login_curve1.png";
+import bottomellipse from "../../assets/Login/login_curve2.png";
+import laptop from "../../assets/Login/loginWorktable.png";
+import logo from "../../assets/Login/mebalogo.png";
+import firstLine from "../../assets/Login/firstLine.png";
+import secondLine from "../../assets/Login/secondLine.png";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="wrapper">
+        <div className="left">
+          <img src={logo} className="logo" />
+          <form className="loginform">
+            <input type="email" placeholder="EMAIL" className="logininput" />
+            <input
+              type="password"
+              placeholder="PASSWORD"
+              className="logininput"
+            />
+            <button className="loginbutton">LOGIN</button>
+          </form>
+          <div className="belowLogin">
+            {/* <img src={firstLine} className="firstLine" /> */}
+            <hr />
+            <div className="or">OR</div>
+            <hr />
+            {/* <img src={secondLine} className="secondLine" /> */}
+          </div>
+          <button className="signupbutton">SIGN UP</button>
+        </div>
+        <div className="right">
+          <img src={laptop} alt="workSpace" className="laptop" />
+        </div>
+      </div>
+      <div className="backgroundimages">
+        <img src={leftellipse} alt="" className="leftEllipse" />
+        <img src={leftellipse2} alt="" className="leftEllipse2" />
+        <img src={rightellipse3} alt="" className="rightEllipse3" />
+        <img src={topellipse} alt="" className="topellipse" />
+        <img src={bottomellipse} alt="" className="bottomellipse" />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
